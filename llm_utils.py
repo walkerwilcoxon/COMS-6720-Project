e8c89bd1834457b97b30c8211803e1410797b290
Walker Wilcoxon <walker.wilcoxon@gmail.com>
2025-11-13 15:25:07 -0600

    Fixed llm_utils

--- llm_utils.py
+++ llm_utils.py
@@ -19,11 +19,16 @@ def load_model(llm_id: str):
         model.config.pad_token_id = tokenizer.pad_token_id
     return model, tokenizer
 
-def produce_proof(model, tokenizer, problem):
+def produce_proof(model, tokenizer, problem, instructions=None):
+    if instructions == None:
+        instructions = """
+            Before producing the Lean 4 code to formally prove the given theorem, provide a detailed proof plan outlining the main proof steps and strategies.
+            The plan should highlight key ideas, intermediate lemmas, and proof structures that will guide the construction of the final formal proof.
+        """.strip()
     prompt = f"""
     Complete the following Lean 4 code:
 
-    ```lean4
+    '''lean4
     import Mathlib
     import Aesop
 
@@ -32,10 +37,9 @@ def produce_proof(model, tokenizer, problem):
     open BigOperators Real Nat Topology Rat
 
     {problem}
-    ```
+    '''
 
-    Before producing the Lean 4 code to formally prove the given theorem, provide a detailed proof plan outlining the main proof steps and strategies.
-    The plan should highlight key ideas, intermediate lemmas, and proof structures that will guide the construction of the final formal proof.
+    {instructions}
     """.strip()
 
     chat = [
@@ -49,6 +53,34 @@ def produce_proof(model, tokenizer, problem):
     output = tokenizer.batch_decode(output_tokens)[0]
     dt = time.time() - start
     return output, dt
+
+def revise_proof(model, tokenizer, proof, feedback, instructions=None):
+    if instructions == None:
+        instructions = """
+            Before producing the Lean 4 code to formally prove the given theorem, provide a detailed proof plan outlining the main proof steps and strategies.
+            The plan should highlight key ideas, intermediate lemmas, and proof structures that will guide the construction of the final formal proof.
+        """.strip()
+    prompt = f"""
+    You are an expert in Lean. Your task is to revise the proof that is provided
+    
+    {proof}
+    '''
+
+    {instructions}
+    """.strip()
+
+    chat = [
+        {"role": "user", "content": prompt},
+    ]
+
+    inputs = tokenizer.apply_chat_template(chat, tokenize=True, add_generation_prompt=True, return_tensors="pt", padding=True, truncation=True).to(model.device)
+
+    start = time.time()
+    output_tokens = model.generate(inputs, max_new_tokens=8192)
+    output = tokenizer.batch_decode(output_tokens)[0]
+    dt = time.time() - start
+    return output, dt
+
 
 def validate_proof(proof) -> dict[str, object]:
     proof_evalutation = None # evaluation
@@ -64,7 +96,7 @@ def extract_problems(file) -> List[str]:
         lines = f.readlines()
         contents = reduce(lambda x, y: x + y, lines)
 
-        regex = re.compile("theorem[\s\S]*?sorry")
+        regex = re.compile(r"theorem[\s\S]*?sorry")
 
         matches = regex.findall(contents)
 
@@ -85,4 +117,3 @@ def get_f2f_problems() -> Dict[str, List[str]]:
         "test": test_problems,
         "valid": valid_problems
     }
-
